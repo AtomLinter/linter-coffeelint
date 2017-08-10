8b54bfb4f712c77cebbf429658f4c923fda02577
Tony Brix <Tony@Brix.ninja>
2017-08-09 21:13:19 -0500

    add config file test

--- spec/linter-coffeelint-spec.js
+++ spec/linter-coffeelint-spec.js
@@ -4,6 +4,8 @@ import { join } from 'path';
 
 const validPath = join(__dirname, 'fixtures', 'valid', 'valid.coffee');
 const arrowSpacingPath = join(__dirname, 'fixtures', 'arrow_spacing', 'arrow_spacing.coffee');
+const arrowSpacingWarningPath = join(__dirname, 'fixtures', 'arrow_spacing_warning', 'arrow_spacing.coffee');
+const arrowSpacingWarningFolder = join(__dirname, 'fixtures', 'arrow_spacing_warning');
 const linter = require('../lib/init.coffee').provideLinter();
 
 describe('The CoffeeLint provider for Linter', () => {
@@ -31,6 +33,20 @@ describe('The CoffeeLint provider for Linter', () => {
             expect(messages[0].severity).toBe('error');
             expect(messages[0].excerpt).toBe(msgText);
             expect(messages[0].location.file).toBe(arrowSpacingPath);
+            expect(messages[0].location.position).toEqual([[6, 0], [6, 12]]);
+          }),
+      );
+    });
+
+    it('uses the config file from the project', () => {
+      const msgText = 'Function arrows (-> and =>) must be spaced properly. (arrow_spacing)';
+      waitsForPromise(() =>
+        atom.workspace.open(arrowSpacingWarningPath).then(editor => linter.lint(editor))
+          .then((messages) => {
+            expect(messages.length).toBe(1);
+            expect(messages[0].severity).toBe('warning');
+            expect(messages[0].excerpt).toBe(msgText);
+            expect(messages[0].location.file).toBe(arrowSpacingWarningPath);
             expect(messages[0].location.position).toEqual([[6, 0], [6, 12]]);
           }),
       );
